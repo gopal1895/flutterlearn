e13fa59000ef06f4c7d8e17c93b4f5c0db438d6b
programmingfile <kumegopal9505@gmail.com>
2021-03-20 20:31:48 +0530

    flutter Hello World

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "MyApp", home: HomePage());
+  }
+}
